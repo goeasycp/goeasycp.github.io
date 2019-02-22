8f0790e1d221ee89310cc7fa61989ec6de242d1b
goeasycp <47539105+goeasycp@users.noreply.github.com>
2019-02-22 15:49:37 -0500

    Create ccgal.js

---
+++ js/ccgal.js
@@ -0,0 +1,5 @@
+$.featherlightGallery.prototype.afterContent = function() {
+  var caption = this.$currentTarget.find('.ccgal-figcaption').html();
+  this.$instance.find('.ccgal-caption').remove();
+  $('<div class="ccgal-caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
+};
