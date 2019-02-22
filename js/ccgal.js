167ed01ce344c11d0b9357ffddcebaa3f180211d
goeasycp <47539105+goeasycp@users.noreply.github.com>
2019-02-22 16:02:45 -0500

    Update ccgal.js

--- js/ccgal.js
+++ js/ccgal.js
@@ -1,5 +1,8 @@
-$.featherlightGallery.prototype.afterContent = function() {
+
+jQuery(document).ready(function () {
+  $.featherlightGallery.prototype.afterContent = function() {
   var caption = this.$currentTarget.find('.ccgal-figcaption').html();
   this.$instance.find('.ccgal-caption').remove();
   $('<div class="ccgal-caption">').text(caption).appendTo(this.$instance.find('.featherlight-content'));
 };
+});
